91fbf4a941c363f7dd49881d00faf5b2b79d3b93
kent <test@gmail.com>
2024-01-04 17:01:39 +0800

    kent_branch latest commit 01-04-2024 5:01PM

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [name, setName]     = useState('John Wick');
+  const [person, setPerson] = useState({
+    name: 'mario',
+    age: 20
+  })
+
+  const clickHandler = () => {
+    setName('Samurai X');
+    setPerson({ name: 'Luigi', age: 20})
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>My name is {name}</Text>
+      <Text>His name is {person.name} and his age is {person.age}</Text>
+      <View style={styles.buttonContainer}>
+        <Button title="Update state" onPress={clickHandler}/>
+      </View>
     </View>
   );
 }
